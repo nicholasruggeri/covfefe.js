971421156b2e2be3bf9add07404ffd55cc509ffa
Nicholas <ruggerinicholas@gmail.com>
2017-06-01 11:57:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function covfefe(pattern){
   Array.from(document.querySelectorAll("body, body *:not(script):not(noscript):not(style)"))
     .forEach(someNode => Array.from(someNode.childNodes)
       .filter(childNode => childNode.nodeType == 3)
-      .forEach(textNode => textNode.textContent = textNode.textContent.replace(pattern, 'covfefe)));
+      .forEach(textNode => textNode.textContent = textNode.textContent.replace(pattern, 'covfefe')));
 }
 
 covfefe('conference');
